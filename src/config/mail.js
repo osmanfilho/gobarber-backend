0fca01eecda5bb61bc59c101521c11fa391968e0
Osman Setuval <osman@ntsaobraz.com.br>
2019-10-31 18:04:25 -0300

    Adição da rotina de agendamento

---
+++ src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 587,
+  secure: false,
+  auth: {
+    user: '01c9f43401438c',
+    pass: '03a07b078de1a4',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// MailTrap (DEV)
